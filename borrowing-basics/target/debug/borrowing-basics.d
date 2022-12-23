53475413e219d6d4c8a4866144e71e810ba4d917
jkarasha@gmail.com <jokarash@LAPTOP-BTS6073A>
2022-12-23 02:04:18 -0800

    Add borrowing base.

---
+++ borrowing-basics/target/debug/borrowing-basics.d
@@ -0,0 +1,1 @@
+/mnt/c/Dev/Rust/learning-rust/borrowing-basics/target/debug/borrowing-basics: /mnt/c/Dev/Rust/learning-rust/borrowing-basics/src/main.rs
---
+++ borrowing-basics/target/debug/deps/borrowing_basics-6af06dfddb38717b.d
@@ -0,0 +1,5 @@
+/mnt/c/Dev/Rust/learning-rust/borrowing-basics/target/debug/deps/borrowing_basics-6af06dfddb38717b: src/main.rs
+
+/mnt/c/Dev/Rust/learning-rust/borrowing-basics/target/debug/deps/borrowing_basics-6af06dfddb38717b.d: src/main.rs
+
+src/main.rs:
