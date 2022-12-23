4d31a6b5cadf7ee06369ef19145aee29c89b6e63
jkarasha@gmail.com <jokarash@LAPTOP-BTS6073A>
2022-12-22 20:15:18 -0800

    Add ownership wth strings.

---
+++ ownership-strings/target/debug/deps/ownership_strings-d95c9f17c20b1414.d
@@ -0,0 +1,5 @@
+/mnt/c/Dev/Rust/learning-rust/ownership-strings/target/debug/deps/ownership_strings-d95c9f17c20b1414: src/main.rs
+
+/mnt/c/Dev/Rust/learning-rust/ownership-strings/target/debug/deps/ownership_strings-d95c9f17c20b1414.d: src/main.rs
+
+src/main.rs:
