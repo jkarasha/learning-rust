246d6ff4c030ba684a2bfd441f6256f6ddfbc05b
jkarasha <jkarasha@gmail.com>
2022-12-24 16:33:31 -0800

    Fix mutable references.

--- mutable-references/src/main.rs
+++ mutable-references/src/main.rs
@@ -3,16 +3,16 @@ struct Bucket {
     liters: u32
 }
 
-fn pour(source: &Bucket, target: &Bucket, amount: u32) {
+fn pour(source: &mut Bucket, target: &mut Bucket, amount: u32) {
     source.liters -= amount;
     target.liters += amount;
 }
 
 fn main() {
-    let source_bucket = Bucket { liters: 15 };
-    let target_bucket = Bucket { liters: 5 };
+    let mut source_bucket = Bucket { liters: 15 };
+    let mut target_bucket = Bucket { liters: 5 };
 
-    pour(&source_bucket, &target_bucket, 5);
+    pour(&mut source_bucket, &mut target_bucket, 5);
 
     println!("Source bucket: {:?}", source_bucket);
     println!("Target bucket: {:?}", target_bucket);
