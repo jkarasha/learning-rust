b4f8429a174a8af91a1c2940d065d67909c8c4ab
jkarasha <jkarasha@gmail.com>
2022-12-24 16:22:42 -0800

    Shows mutation error on reference.

---
+++ mutable-references/target/debug/deps/mutable_references-71fd054657e1d5b9.d
@@ -0,0 +1,5 @@
+/mnt/c/Dev/Rust/learning-rust/mutable-references/target/debug/deps/mutable_references-71fd054657e1d5b9: src/main.rs
+
+/mnt/c/Dev/Rust/learning-rust/mutable-references/target/debug/deps/mutable_references-71fd054657e1d5b9.d: src/main.rs
+
+src/main.rs:
