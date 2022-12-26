ba2f237fe1454a67f92df5e211d1128f6a7c6ee4
jkarasha <jkarasha@gmail.com>
2022-12-26 12:35:46 -0800

    Shows iterator invalidation error.

--- mutable-references/src/main.rs
+++ mutable-references/src/main.rs
@@ -1,24 +1,12 @@
-#[derive(Debug)]
 
-struct CarPool {
-    passengers: Vec<String>
-}
-
-impl CarPool {
-    //in order to update the object state, we have to make self mutable.
-    fn pick_up(&mut self, name: String) {
-        self.passengers.push(name);
-    }
-}
+// Show the iterator invalidation problem in rust.
 fn main() {
-
-    let mut monday_car_pool = CarPool {
-        passengers: vec![],
-    };
-
-    monday_car_pool.pick_up(String::from("Joe"));
-    println!("Current carpool state {:?}", monday_car_pool);
-
-    monday_car_pool.pick_up(String::from("Jessy"));
-    println!("Current carpool state {:?}", monday_car_pool);
+    let mut list = vec![1, 2, 3];
+    // although list is mutable, we won'be be able to modify it without making a copy/slice?
+    // Not sure I understand fully at this time
+    for i in &list {
+        println!("i is {}", i);
+        // here we try to add something to the list
+        list.push(i + 1);
+    }
 }
