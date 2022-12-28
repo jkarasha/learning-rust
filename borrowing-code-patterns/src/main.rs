8ce51a6144fdebcf64acaae70d555fa36737fd27
jkarasha <jkarasha@gmail.com>
2022-12-28 12:21:40 -0800

    Temp variable to narrow borrowing scope

--- borrowing-code-patterns/src/main.rs
+++ borrowing-code-patterns/src/main.rs
@@ -1,8 +1,11 @@
 fn main() {
-    let list = vec![1,2,3];
+    let mut list = vec![1, 2, 3];
 
     let list_first = list.first();
     let list_last = list.last();
+
+    let list_first_mut = list.first_mut().expect("list was empty");
+    *list_first_mut += 1;
 
     println!(
         "The first element is {:?} and the last is {:?}",
