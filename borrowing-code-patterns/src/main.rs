c2aa7ec52fb07e9853980c9fea3945986063b6f9
jkarasha <jkarasha@gmail.com>
2022-12-29 15:32:22 -0800

    Use explicit scope to seperate immutable and mutable borrows.

--- borrowing-code-patterns/src/main.rs
+++ borrowing-code-patterns/src/main.rs
@@ -3,27 +3,26 @@
 fn main() {
     let mut list = vec![1, 2, 3];
 
-    // example of a mutable borrow
-    // mutable borrow starts and ends on this line.
-    // Notice the location of this code matters.
-    // It happens before the immutable borrows.
+    {
+        // example of immutable borrows with explicit scope definition.
+        // We want to be explicit in our intent
+        // These immutable borrows scope last only for the nested scope duration.
+        let list_first = list.first();
+        let list_last = list.last();
+
+
+        // We are using both mutable and immutable borrows
+        // Why doesn't this example throw E0502?
+        // Because the mutation happens inline, no variable is left holding on to the value.
+        
+        println!(
+            "The first element is {:?} and the last is {:?}",
+            list_first,
+            list_last,
+        );
+    }
+
+    // Since we explicity defined the scope of the immutable borrows, this code should work
     *list.first_mut().expect("list was empty") += 1;
 
-    // example of immutable borrows.
-    let list_first = list.first();
-    let list_last = list.last();
-
-    // IF we tried to line line 15 here instead, it would fail.
-    // Because it's trying to mutable borrow of an already immutable borrowed object.
-    //*list.first_mut().expect("list was empty") += 1;
-
-    // We are using both mutable and immutable borrows
-    // Why doesn't this example throw E0502?
-    // Because the mutation happens inline, no variable is left holding on to the value.
-    
-    println!(
-        "The first element is {:?} and the last is {:?}",
-        list_first,
-        list_last,
-    );
 }
