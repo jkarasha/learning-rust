fac4aca084eb6600a5b22ace57f47fb03f56dbe1
jkarasha <jkarasha@gmail.com>
2022-12-30 11:21:58 -0800

    String interpolation on panic.

--- error-handling/src/main.rs
+++ error-handling/src/main.rs
@@ -1,5 +1,8 @@
 //error handling using panic macro.
 fn main() {
     //Will throw error/exception: thread 'main' panicked at 'Something's not right here', src/main.rs:3:5
-    panic!("Something's not right here");
+    // panic!("Something's not right here");
+    // You can also interpolate string values when calling panic.
+    let age = "unknown";
+    panic!("Something's not right here. We got the value '{}' for age!", age);
 }
