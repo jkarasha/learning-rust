d5b36d25c7bd5737716931aad4421e7a7b4e0cdf
jkarasha <jkarasha@gmail.com>
2022-12-30 11:53:12 -0800

    Show index-out-of-range

--- error-handling/src/main.rs
+++ error-handling/src/main.rs
@@ -1,27 +1,8 @@
-#[derive(Debug)]
-enum Platform {
-    Windows,
-    Linux,
-    MacOS
-}
 
-impl Platform {
-    fn parse(platform_arg: &str) -> Platform {
-        if platform_arg == "windows" {
-            Platform::Windows
-        } else if platform_arg == "linux" {
-            Platform::Linux
-        } else if platform_arg == "mac" {
-            Platform::MacOS
-        } else {
-            panic!("unknown platform: {}. Valid values: [windows, linux, mac].", platform_arg);
-        }
-    }
-}
 
 fn main() {
-    //defaults to windows..
-    let platform_arg = "macos";
-    let platform = Platform::parse(platform_arg);
-    println!("Producing output for {:?}.", platform);
+    let v = vec![1, 2, 3];
+    let index = 50;
+    //expected error: thread 'main' panicked at 'index out of bounds: the len is 3 but the index is 50', src/main.rs:7:53
+    println!("The value at index {} is {}.", index, v[index]);
 }
