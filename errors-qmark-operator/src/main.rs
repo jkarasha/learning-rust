dd0444522cef0efb5c831dc74cccb4dedf7267d6
jkarasha <jkarasha@gmail.com>
2023-01-02 13:22:34 -0800

    Cannot use the ? operator in a function that returns i32.

--- errors-qmark-operator/src/main.rs
+++ errors-qmark-operator/src/main.rs
@@ -1,32 +1,14 @@
-// using the question mark operator to handle Result statuses.
-//'static str -> will return a static string
-fn save_status(text: &str) -> Result<i64, &'static str> {
-    if text.len() > 200 {
-        return Err("Status text is too long!")
-    }
 
-    // the question mark operator was build to solve for this pattern
-    // the match below can be replaced with a single line.
-    /* let record = match save_to_database(text) {
-        Ok(rec) => rec,
-        Err(e) => return Err(e),
-    }; */
-    let record = save_to_database(text)?;
-
-    Ok(record.id)
-}
-
-fn save_to_database(text: &str)  -> Result<StatusRecord, &'static str> {
-    // fake db implementation that always returns an error
-    Err("Database is not available!")
-}
-
-struct StatusRecord {
-    id: i64,
-    text: String,
-    created_at: std::time::Instant,
+fn parse_number(n: &str) -> i32 {
+    //parse returns a Result<> type
+    // expected `i32`, found enum `Result`
+    //let num: i32 = n.parse();
+    // if we try to use ? it will also fail.
+    // cannot use the `?` operator in a function that returns `i32`
+    let num: i32 = n.parse()?;
+    num
 }
 
 fn main() {
-    println!("{:?}", save_status("Boo!"));
+    println!("Number: ", parse_number("three"));
 }
