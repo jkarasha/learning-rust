0b4d83863507499633de0997f1e4db9443fca4fd
jkarasha <jkarasha@gmail.com>
2023-01-03 17:22:57 -0800

    Show function with multiple error types.

--- custom-errors/src/main.rs
+++ custom-errors/src/main.rs
@@ -1,3 +1,12 @@
+//many times you have a function that can return different types of errors.
+//What will the error return type be??
+fn num_threads() -> Result<usize, ???> {
+    //below line will return a VarErr??
+    let s = env::var("NUM_THREADS")?;
+    //below line will return IntParseError
+    let n: usize = s.parse()?;
+    Ok(n + 1)
+}
+
 fn main() {
-    println!("Hello, world!");
 }
