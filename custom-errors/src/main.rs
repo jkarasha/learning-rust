2fb76256dc129cd528878d62c606362543183cc9
jkarasha <jkarasha@gmail.com>
2023-01-04 19:55:53 -0800

    Add custom DocumentServiceError.

--- custom-errors/src/main.rs
+++ custom-errors/src/main.rs
@@ -2,6 +2,7 @@
 use std::error::Error;
 use std::fs::File;
 use std::fs::OpenOptions;
+use std::io;
 
 const MAX_DOCS_CREATED_PER_MINUTE: u8 = 100;
 
@@ -9,6 +10,11 @@ fn num_documents_created_in_last_minute() -> u8 {
     2
 }
 
+pub enum DocumentServiceError {
+    RateLimitExceeded,
+    Io(io::Error),    
+}
+//Why is this declared at public? Works either way
 pub fn create_document(filename: &str) -> Result<File, Box<dyn Error>> {
     if num_documents_created_in_last_minute() > MAX_DOCS_CREATED_PER_MINUTE {
         return Err("You have exceeded allowed docs per minute!".into());
