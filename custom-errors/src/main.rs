619b79e1d0e53cfc6dc3e2c08b38f1420d31a2cb
jkarasha <jkarasha@gmail.com>
2023-01-04 19:48:47 -0800

    Custom error using file handlign example..

--- custom-errors/src/main.rs
+++ custom-errors/src/main.rs
@@ -1,15 +1,27 @@
-//many times you have a function that can return different types of errors.
+//using custom errors
 use std::error::Error;
-use std::env;
-//We'll use Box<Error> to handle multiple errors in same function..
-fn num_threads() -> Result<usize, Box<dyn Error>> {
-    //below line will return a VarErr??
-    let s = env::var("NUM_THREADS")?;
-    //below line will return IntParseError
-    let n: usize = s.parse()?;
-    Ok(n + 1)
+use std::fs::File;
+use std::fs::OpenOptions;
+
+const MAX_DOCS_CREATED_PER_MINUTE: u8 = 100;
+
+fn num_documents_created_in_last_minute() -> u8 {
+    2
 }
 
-fn main() {
-    println!("{:?}", num_threads());
+pub fn create_document(filename: &str) -> Result<File, Box<dyn Error>> {
+    if num_documents_created_in_last_minute() > MAX_DOCS_CREATED_PER_MINUTE {
+        return Err("You have exceeded allowed docs per minute!".into());
+    }
+
+    let file = OpenOptions::new()
+        .write(true)
+        .create_new(true)
+        .open(filename)?;
+
+    Ok(file)
+}
+
+fn main () {
+    println!("{:?}", create_document("docs.txt"));
 }
