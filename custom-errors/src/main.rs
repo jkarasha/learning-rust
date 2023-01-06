17acaac78f3b3301cbea0579666f90f17d8a7e50
jkarasha <jkarasha@gmail.com>
2023-01-05 21:04:02 -0800

    Add comments for describing what we implemented.

--- custom-errors/src/main.rs
+++ custom-errors/src/main.rs
@@ -18,6 +18,10 @@ pub enum DocumentServiceError {
     Io(io::Error),    
 }
 
+//custom error handlers must implement the error trait
+//easily done by adding a definition for the description function as show below.
+//rust versions newer that 1.27 don't have to implement the method body
+//impl Error for DocumentServiceError {} is good enough
 impl Error for DocumentServiceError {
     fn description(&self) -> &str {
         use DocumentServiceError::*;
@@ -28,6 +32,7 @@ impl Error for DocumentServiceError {
     }
 }
 
+//implementing Error trait also requires we implement debug/display traits
 impl fmt::Display for DocumentServiceError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         use DocumentServiceError::*;
@@ -44,7 +49,10 @@ impl fmt::Display for DocumentServiceError {
     }
 }
 
+//implementing the From trait allows us to convert from one type to another
+//in this case we can convert from io:Error to DocumentService:Io Error.
 impl From<io::Error> for DocumentServiceError {
+    // Self here indicates we are returning a DocumentServiceError type
     fn from(other: io::Error) -> Self {
         DocumentServiceError::Io(other)
     }
@@ -52,6 +60,8 @@ impl From<io::Error> for DocumentServiceError {
 
 use std::result;
 
+//create an alias for the Result<T> that returns a DocumentServiceError
+//will greatly simply the code in our "crate" since most functions return this type.
 pub type Result<T> = result::Result<T, DocumentServiceError>;
 
 //Why is this declared at public? Works either way
@@ -63,11 +73,14 @@ pub fn create_document(filename: &str) -> Result<File> {
     let file = OpenOptions::new()
         .write(true)
         .create_new(true)
+        // because we implemented the from trait, ? now handles the type conversion for us
         .open(filename)?;
 
     Ok(file)
 }
 
+// Notice the DocumentServiceError type now isn't explicitly called
+//that's because we defined an alias for our result type
 pub fn archive_document(filenam: &str) -> Result<()> {
     unimplemented!();
 }
