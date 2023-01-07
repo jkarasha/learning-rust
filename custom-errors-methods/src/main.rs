87635b80ab480d3f51399052f9ecf255e9a5f85d
jkarasha <jkarasha@gmail.com>
2023-01-07 14:51:24 -0800

    Simple demo showing how to unwrap a value.

--- custom-errors-methods/src/main.rs
+++ custom-errors-methods/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    //wraps 25 inside of the option value
+    let option_value = Some(25);
+    if option_value.is_some() {
+        //unwrap the option value and extract 25
+        let inner = option_value.unwrap();
+        println!("Inner: {}", inner);
+    }
 }
