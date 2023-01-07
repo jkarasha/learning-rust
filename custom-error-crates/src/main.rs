5727468f019053972f456469d5eec085c66095ec
jkarasha <jkarasha@gmail.com>
2023-01-06 21:22:35 -0800

    update to use failure crate.

--- custom-error-crates/src/main.rs
+++ custom-error-crates/src/main.rs
@@ -1,48 +1,26 @@
-
-//replace with error_chain
-//extern crate quick_error;
+extern crate failure;
 #[macro_use]
-extern crate error_chain;
+extern crate failure_derive;
 
+use failure::{Backtrace, Fail};
 use std::fs::{File, OpenOptions};
-
-// removed
-/*
-use quick_error::ResultExt;
 use std::{io, result};
 
 type Result<T> = result::Result<T, DocumentServiceError>;
-*/
-// replace with error_chain
-/*
-quick_error! {
-    #[derive(Debug)]
-    pub enum DocumentServiceError {
-        RateLimitExceeded {
-            display("You have exceeded the allowed number of documents per minute!")
-        }
-        Io(filename: String, cause: io::Error) {
-            display("I/O error: {} for filename {}", cause, filename)
-            context(filename: &'a str, cause: io::Error)
-                -> (filename.to_string(), cause)
-        }
-    }
-}
-*/
-pub mod errors {
-    error_chain! {
-        errors {
-            RateLimitExceeded {
-                display("You have exceeded the allowable number of documents per minute.")
-            }
-        }
-        foreign_links {
-            Io(::std::io::Error);
-        }
-    }
+
+#[derive(Debug, Fail)]
+pub enum DocumentServiceError {
+    #[fail(display = "You have exceeded the allowed number of documents per minute.")]
+    RateLimitExceeded(Backtrace),
+    #[fail(display = "I/O Error: {}", _0)]
+    Io(io::Error, Backtrace),
 }
 
-use errors::*;
+impl From<io::Error> for DocumentServiceError {
+    fn from(other: io::Error) -> Self {
+        DocumentServiceError::Io(other, Backtrace::new())
+    }
+}
 
 const MAX_DOCS_CREATED_PER_MINUTE: u8 = 100;
 
@@ -52,14 +30,13 @@ fn num_documents_created_in_last_minute() -> u8 {
 
 pub fn create_document(filename: &str) -> Result<File> {
     if num_documents_created_in_last_minute() > MAX_DOCS_CREATED_PER_MINUTE {
-        bail!(ErrorKind::RateLimitExceeded);
+        return Err(DocumentServiceError::RateLimitExceeded(Backtrace::new()))
     }
 
     let file = OpenOptions::new()
         .write(true)
         .create_new(true)
-        .open(filename)
-        .chain_err(|| format!("could not open {}", filename))?;
+        .open(filename)?;
 
     Ok(file)
 }
@@ -77,11 +54,10 @@ fn main() {
         Ok(()) => println!("Project created successfully!"),
         Err(e) => {
             println!("Project creation failed: {}", e);
-            for e in e.iter().skip(1) {
-                println!("Caused by: {}", e);
-            }
             if let Some(backtrace) = e.backtrace() {
-                println!("Backtrace: {:?}", backtrace);
+                if !backtrace.to_string().trim().is_empty() {
+                    println!("Backtrace: {:?}", backtrace);
+                }
             }
         }
     }
