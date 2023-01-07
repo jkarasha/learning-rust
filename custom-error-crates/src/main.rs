493001f6805466054623caea656d4a07bb0e0c5d
jkarasha <jkarasha@gmail.com>
2023-01-06 19:48:32 -0800

    Update to use quick_error crate.

--- custom-error-crates/src/main.rs
+++ custom-error-crates/src/main.rs
@@ -1,18 +1,37 @@
 //use the DocumentServiceError as starting point
 //replace functionality with error crates where relevant
-use std::error::Error;
+
+//we don't need to use the std::error, we'll use quick-error instead
+//use std::error::Error;
+#[macro_use]
+extern crate quick_error;
+
 use std::fs::{File, OpenOptions};
-use std::{fmt, io, result};
+use std::{io, result};
 
 type Result<T> = result::Result<T, DocumentServiceError>;
 
-#[derive(Debug)]
-pub enum DocumentServiceError {
-    RateLimitExceeded,
-    Io(io::Error),
+//wrap the DocumentServiceError enum with the quick_error macro
+quick_error! {
+    #[derive(Debug)]
+    pub enum DocumentServiceError {
+        //update the RateLimitExceeded & Io(io::error) call
+        /*RateLimitExceeded,
+        Io(io::Error),
+        */
+        RateLimitExceeded {
+            display("You have exceeded the allowed number of documents per minute!")
+        }
+        Io(cause: io::Error) {
+            display("I/O error: {}", cause)
+            from()
+        }
+    }
 }
 
-//implement Error trait
+
+//no longer required
+/*
 impl Error for DocumentServiceError {
     fn description(&self) -> &str {
         use DocumentServiceError::*;
@@ -22,8 +41,10 @@ impl Error for DocumentServiceError {
         }
     }
 }
+*/
 
-//implement Format trait
+//no longer required
+/*
 impl fmt::Display for DocumentServiceError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         use DocumentServiceError::*;
@@ -36,14 +57,16 @@ impl fmt::Display for DocumentServiceError {
         }
     }
 }
+*/
 
-//implement From trait
+//no longer required
+/*
 impl From<io::Error> for DocumentServiceError {
     fn from(other: io::Error) -> Self {
         DocumentServiceError::Io(other)
     }
 }
-
+*/
 const MAX_DOCS_CREATED_PER_MINUTE: u8 = 100;
 
 fn num_documents_created_in_last_minute() -> u8 {
