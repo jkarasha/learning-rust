cf638105fae13240a6ad44a93dc4640b9db582ef
jkarasha <jkarasha@gmail.com>
2023-01-08 08:40:22 -0800

    Lifetimes demo across functions.

--- concrete-lifetimes/src/main.rs
+++ concrete-lifetimes/src/main.rs
@@ -1,15 +1,11 @@
 fn main() {
     let list = vec![100,200, 300, 400, 500];
-    // use inner scope to change first-two lifetime
-    {
-        println!("List contents: {:?}", list);
-        let first_two = &list[0..2];
-        println!("First two elements: {:?}", first_two);
-    }
-    //
+    print_first_two(&list);
     println!("List contents: {:?}", list);
-    //attempting to print first_two will throw error
-    //error[E0425]: cannot find value `first_two` in this scope
-    //println!("Re-Print the first two elements: {:?}", first_two);
-    //the lifetime of list lasts through end of main.
+    print_first_two(&list);
+}
+
+fn print_first_two(borrowed_list: &[i32]) {
+    let first_two = &borrowed_list[0..2];
+    println!("First two elements: {:?}", first_two);
 }
