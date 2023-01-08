5ee1f7b1fed0d210fb6afd1d3ad56a2aae3636f7
jkarasha <jkarasha@gmail.com>
2023-01-08 08:32:42 -0800

    Variable lifetimes last through end of main.

--- concrete-lifetimes/src/main.rs
+++ concrete-lifetimes/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let list = vec![100,200, 300, 400, 500];
+    println!("List contents: {:?}", list);
+    //creates a reference to the first two elements of list
+    let first_two = &list[0..2];
+    println!("First two elements: {:?}", first_two);
+    println!("List contents: {:?}", list);
+    println!("Re-Print the first two elements: {:?}", first_two);
+    //the lifetime of list and first_two last through end of main.
 }
