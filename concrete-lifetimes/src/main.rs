081a069cae467f44155ab317ebdccf1ff74b4057
jkarasha <jkarasha@gmail.com>
2023-01-08 08:36:38 -0800

    Use inner scope to alter lifetime.

--- concrete-lifetimes/src/main.rs
+++ concrete-lifetimes/src/main.rs
@@ -1,10 +1,15 @@
 fn main() {
     let list = vec![100,200, 300, 400, 500];
+    // use inner scope to change first-two lifetime
+    {
+        println!("List contents: {:?}", list);
+        let first_two = &list[0..2];
+        println!("First two elements: {:?}", first_two);
+    }
+    //
     println!("List contents: {:?}", list);
-    //creates a reference to the first two elements of list
-    let first_two = &list[0..2];
-    println!("First two elements: {:?}", first_two);
-    println!("List contents: {:?}", list);
-    println!("Re-Print the first two elements: {:?}", first_two);
-    //the lifetime of list and first_two last through end of main.
+    //attempting to print first_two will throw error
+    //error[E0425]: cannot find value `first_two` in this scope
+    //println!("Re-Print the first two elements: {:?}", first_two);
+    //the lifetime of list lasts through end of main.
 }
