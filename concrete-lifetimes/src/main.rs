456ab3112aba64c98462ccdd5ac6fdb019ce30f3
jkarasha <jkarasha@gmail.com>
2023-01-09 21:44:08 -0800

    Set up module 3.

--- concrete-lifetimes/src/main.rs
+++ concrete-lifetimes/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Start with module 3!")
+    let first_two = {
+        let list = vec![100, 200, 300, 400];
+        &list[0..2]
+    };
+
+    println!("The first two elements are: {:?}", first_two)
 }
