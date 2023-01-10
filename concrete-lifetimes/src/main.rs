bc7621ebf33ffffe6152abc38cc0b8aeced48cf7
jkarasha <jkarasha@gmail.com>
2023-01-09 21:40:05 -0800

    Setup for module three.

--- concrete-lifetimes/src/main.rs
+++ concrete-lifetimes/src/main.rs
@@ -1,19 +1,3 @@
 fn main() {
-    let list = vec![100,200, 300, 400, 500];
-    println!("List contents: {:?}", list);
-    //not the same first_two defined in print_first_two()
-    let first_two = print_first_two_return(&list);
-    println!("First two elements: {:?}", first_two);  
-    //
-    print_first_two(&list);
-}
-
-fn print_first_two(borrowed_list: &[i32]) {
-    //first_two lifetime is limited to this function.
-    let first_two = &borrowed_list[0..2];
-    println!("First two elements: {:?}", first_two);
-}
-
-fn print_first_two_return(borrowed_list: &[i32]) -> &[i32] {
-    &borrowed_list[0..2]
+    println!("Start with module 3!")
 }
