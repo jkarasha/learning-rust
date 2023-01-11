21110043db1494e976e69c0304d1bdcc599114e6
jkarasha <jkarasha@gmail.com>
2023-01-10 19:47:51 -0800

    Fix issue returning a ref from a function.

--- concrete-lifetimes/src/main.rs
+++ concrete-lifetimes/src/main.rs
@@ -1,12 +1,11 @@
 fn main() {
-    //tries to retun a reference from a function.
-    //error[E0106]: missing lifetime specifier
-    let first_two = return_first_two();
+    //creates list in main function
+    let list = return_first_two();
+    let first_two = &list[0..2];
+    //lifetime is now valid
     println!("The first two elements are: {:?}", first_two)
 }
 
-fn return_first_two() -> &[i32] {
-    //because list is defined in the function, a slice of list won't be available outside of function.
-    let list = vec![100, 200, 300, 400];
-    &list[0..2]
+fn return_first_two() -> Vec<i32> {
+    vec![100, 200, 300, 400]
 }
