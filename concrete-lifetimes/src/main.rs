4ef86c3566564e6abbd88a60edecc0b1735fda63
jkarasha <jkarasha@gmail.com>
2023-01-10 19:52:28 -0800

    Attempts to use a moved value.

--- concrete-lifetimes/src/main.rs
+++ concrete-lifetimes/src/main.rs
@@ -1,11 +1,9 @@
 fn main() {
-    //creates list in main function
-    let list = return_first_two();
-    let first_two = &list[0..2];
-    //lifetime is now valid
+    //attempts to use a moved value
+    let list_a = vec![100,200,300,400];
+    let list_b = list_a;
+    //try to access value from moved list_a
+    //error[E0382]: borrow of moved value: `list_a`
+    let first_two = &list_a[0..2];
     println!("The first two elements are: {:?}", first_two)
-}
-
-fn return_first_two() -> Vec<i32> {
-    vec![100, 200, 300, 400]
 }
