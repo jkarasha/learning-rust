c0683f0bc6a88b89026cb0fce758f338b96352fd
jkarasha <jkarasha@gmail.com>
2023-01-10 19:15:37 -0800

    Attempts to use a borrowed value from an inner scope.

--- concrete-lifetimes/src/main.rs
+++ concrete-lifetimes/src/main.rs
@@ -4,5 +4,7 @@ fn main() {
         &list[0..2]
     };
 
+    //attempting to use a value from an inner scope
+    //borrowed value does not live long enough
     println!("The first two elements are: {:?}", first_two)
 }
