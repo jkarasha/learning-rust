f732042a063c056f0ff44b6bc37a67ea3f2505de
jkarasha <jkarasha@gmail.com>
2023-01-10 20:03:10 -0800

    Fix for referencing moved value.

--- concrete-lifetimes/src/main.rs
+++ concrete-lifetimes/src/main.rs
@@ -1,9 +1,10 @@
 fn main() {
-    //attempts to use a moved value
     let list_a = vec![100,200,300,400];
+    {
+        let first_two = &list_a[0..2];
+        println!("The first two elements are: {:?}", first_two)
+    }
+    // move after use/assign
     let list_b = list_a;
-    //try to access value from moved list_a
-    //error[E0382]: borrow of moved value: `list_a`
-    let first_two = &list_a[0..2];
-    println!("The first two elements are: {:?}", first_two)
+    println!("The first two elements are: {:?}", list_b)
 }
