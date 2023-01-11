6442d4d7a7350ce96e3cb963c5178e427026e0c3
jkarasha <jkarasha@gmail.com>
2023-01-10 19:43:16 -0800

    Attempts to return value from a function.

--- concrete-lifetimes/src/main.rs
+++ concrete-lifetimes/src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
-    //one way to fix previous issue, by moving everything in the same scope
-    let list = vec![100, 200, 300, 400];
-    let first_two = &list[0..2];
+    //tries to retun a reference from a function.
+    //error[E0106]: missing lifetime specifier
+    let first_two = return_first_two();
     println!("The first two elements are: {:?}", first_two)
+}
+
+fn return_first_two() -> &[i32] {
+    //because list is defined in the function, a slice of list won't be available outside of function.
+    let list = vec![100, 200, 300, 400];
+    &list[0..2]
 }
