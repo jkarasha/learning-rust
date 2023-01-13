ecc5569432970032c4167357ad3e6b1671e71218
jkarasha <jkarasha@gmail.com>
2023-01-12 21:07:31 -0800

    Missing lifetime specifier on function call.

--- intro-generics/src/main.rs
+++ intro-generics/src/main.rs
@@ -1,3 +1,22 @@
+extern crate rand;
+
+//this will generate error[E0106]: missing lifetime specifier error
+// rust has no way to guaranteeing returned value [home|away] will be around long enough
+fn simulate_game(home: &str, away: &str) -> &str {
+    //randomlyre return winner
+    if rand::random() {
+        home
+    } else  {
+        away
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let team_one = String::from("Patriots");
+    {
+        let team_two = String::from("Bills");
+        let winner = simulate_game(&team_one, &team_two);
+        println!("Final match between {} and {}: {} won!", team_one, team_two, winner);
+    }
+    println!("I think {} should have won!", team_one);
 }
