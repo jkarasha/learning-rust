e40b4f80ae32ac99320ce85953099e5aef32f18e
jkarasha <jkarasha@gmail.com>
2023-01-12 21:14:53 -0800

    Changed scoped, but will still fail since team_two is not guaranteed to be around.

--- intro-generics/src/main.rs
+++ intro-generics/src/main.rs
@@ -3,7 +3,7 @@ extern crate rand;
 //this will generate error[E0106]: missing lifetime specifier error
 // rust has no way to guaranteeing returned value [home|away] will be around long enough
 fn simulate_game(home: &str, away: &str) -> &str {
-    //randomlyre return winner
+    //randomly return winner
     if rand::random() {
         home
     } else  {
@@ -13,10 +13,13 @@ fn simulate_game(home: &str, away: &str) -> &str {
 
 fn main() {
     let team_one = String::from("Patriots");
-    {
+    //because team two is defined in nested scope,
+    //we have no guarantee value will be around by the time we print
+    //this causes this code to also fail. Same error error[E0106]: missing lifetime specifier
+    let winner = {
         let team_two = String::from("Bills");
-        let winner = simulate_game(&team_one, &team_two);
-        println!("Final match between {} and {}: {} won!", team_one, team_two, winner);
-    }
-    println!("I think {} should have won!", team_one);
+        simulate_game(&team_one, &team_two)
+    };
+    //println!("Final match between {} and {}: {} won!", team_one, team_two, winner);
+    println!("I think {} should have won!", winner);
 }
