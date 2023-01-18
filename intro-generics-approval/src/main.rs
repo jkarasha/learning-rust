a270e000125f576419b8d3055c774f3711649358
jkarasha <jkarasha@gmail.com>
2023-01-17 19:33:08 -0800

    Add unimplemented!() method signatures.

--- intro-generics-approval/src/main.rs
+++ intro-generics-approval/src/main.rs
@@ -1,3 +1,24 @@
+//define a generic struct
+pub struct Approval<T> {
+    item: T,
+    approved: bool,
+}
+
+impl<T> Approval<T> {
+    pub fn new(item: T) -> Approval<T> {
+        unimplemented!()
+    }
+    pub fn approve(&mut self) {
+        unimplemented!()
+    }
+    pub fn replace(self, other_item: T) -> Approval<T> {
+        unimplemented!()
+    }
+    pub fn approved_item(&self) -> Option<&T> {
+        unimplemented!()
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
