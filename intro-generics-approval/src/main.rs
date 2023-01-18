4c920146136517419dba11764f398ea406ce8ea4
jkarasha <jkarasha@gmail.com>
2023-01-17 19:45:33 -0800

    Test using different item (IPAddress vs Amount).

--- intro-generics-approval/src/main.rs
+++ intro-generics-approval/src/main.rs
@@ -38,4 +38,12 @@ fn main() {
     approval_amount.approve();
     assert_eq!(approval_amount.approved_item(), Some(&1000));
     //
+    let mut approval_ip = Approval::new(IpAddr::V4(Ipv4Addr::new(127,0,0,1)));
+    assert!(approval_ip.approved_item().is_none());
+    approval_ip.approve();
+    assert_eq!(
+        approval_ip.approved_item(),
+        Some(&IpAddr::V4(Ipv4Addr::new(127,0,0,1)))
+    );
+    println!("Validation complete!");
 }
