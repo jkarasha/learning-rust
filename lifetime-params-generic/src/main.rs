0b50cb243a63f02ee43549d2e1636c0ba006a8bd
jkarasha <jkarasha@gmail.com>
2023-01-18 20:37:08 -0800

    Show missing lifetime specifier.

--- lifetime-params-generic/src/main.rs
+++ lifetime-params-generic/src/main.rs
@@ -1,3 +1,16 @@
+extern crate rand;
+
+//will throw error[E0106]: missing lifetime specifier
+//help: this function's return type contains a borrowed value, 
+//but the signature does not say whether it is borrowed from `home` or `away`
+fn simulate_game(home: &str, away: &str) -> &str {
+    if rand::random() {
+        home
+    } else {
+        away
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
