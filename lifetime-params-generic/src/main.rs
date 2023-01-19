592474f065b023b98fd9684537cbc8952c3bf556
jkarasha <jkarasha@gmail.com>
2023-01-18 20:44:43 -0800

    Use nested scope to show limited lifetime. This won't compile

--- lifetime-params-generic/src/main.rs
+++ lifetime-params-generic/src/main.rs
@@ -10,5 +10,13 @@ fn simulate_game<'a>(home: &'a str, away: &'a str) -> &'a str {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let team_one = String::from("Patriots");
+    {
+        let team_two = String::from("Bills");
+        let winner = simulate_game(&team_one, &team_two);
+        println!("{} vs. {}: {} won", team_one, team_two, winner);
+    }
+    //if you tried to print the winner here, code will fail
+    //winner's lifetime is only within nested scope.
+    println!("I can't believe {} won", winner);
 }
