431e3767dc3b5e24adc5da81cf86d399aa736ed3
jkarasha <jkarasha@gmail.com>
2023-01-18 20:40:22 -0800

    Adding tick-a ('a) to resolve lifetime specifier error. Code now compiles.

--- lifetime-params-generic/src/main.rs
+++ lifetime-params-generic/src/main.rs
@@ -1,9 +1,7 @@
 extern crate rand;
 
-//will throw error[E0106]: missing lifetime specifier
-//help: this function's return type contains a borrowed value, 
-//but the signature does not say whether it is borrowed from `home` or `away`
-fn simulate_game(home: &str, away: &str) -> &str {
+//adding 'a (tick-a) allows the code to compile.
+fn simulate_game<'a>(home: &'a str, away: &'a str) -> &'a str {
     if rand::random() {
         home
     } else {
