4f8179aced1cc7d646d9e68c5f843b30588fbfb7
jkarasha <jkarasha@gmail.com>
2023-01-19 16:34:01 -0800

    Attempt resolving error by adding 'a(tick-a).

--- lifetime-params-descriptive/src/main.rs
+++ lifetime-params-descriptive/src/main.rs
@@ -4,12 +4,9 @@ fn main() {
     println!("First two elements are {:?}", first_two);
 }
 
-//won't compile: error[E0106]: missing lifetime specifier
-//&[i32] return type expected named lifetime parameter
-//help: this function's return type contains a borrowed value, 
-//but there is no value for it to be borrowed from
-help: consider using the `'static` lifetime
-fn return_first_two() -> &[i32] {
+//adding tick-a 'a, resolves initial error
+//error[E0515]: cannot return value referencing local variable `list`
+fn return_first_two<'a>() -> &'a [i32] {
     let list = vec![100,200,300,400];
     &list[0..2]
 }
