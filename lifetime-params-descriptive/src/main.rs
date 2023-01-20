a7bda4b346458a2c75a687452a227048efc543a7
jkarasha <jkarasha@gmail.com>
2023-01-19 16:31:17 -0800

    Won't compile, expects named lifetime parameter.

--- lifetime-params-descriptive/src/main.rs
+++ lifetime-params-descriptive/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let first_two = return_first_two();
+
+    println!("First two elements are {:?}", first_two);
+}
+
+//won't compile: error[E0106]: missing lifetime specifier
+//&[i32] return type expected named lifetime parameter
+//help: this function's return type contains a borrowed value, 
+//but there is no value for it to be borrowed from
+help: consider using the `'static` lifetime
+fn return_first_two() -> &[i32] {
+    let list = vec![100,200,300,400];
+    &list[0..2]
 }
