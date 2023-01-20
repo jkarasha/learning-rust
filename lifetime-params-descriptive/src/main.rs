9efb9d79b8c74721abb9ec9e0b052359adefd119
jkarasha <jkarasha@gmail.com>
2023-01-19 16:36:21 -0800

    Try resolving by adding 'static (static-lifetime).

--- lifetime-params-descriptive/src/main.rs
+++ lifetime-params-descriptive/src/main.rs
@@ -4,9 +4,9 @@ fn main() {
     println!("First two elements are {:?}", first_two);
 }
 
-//adding tick-a 'a, resolves initial error
+//instead 'a trying adding 'static (static-lifetime)
 //error[E0515]: cannot return value referencing local variable `list`
-fn return_first_two<'a>() -> &'a [i32] {
+fn return_first_two() -> &'static [i32] {
     let list = vec![100,200,300,400];
     &list[0..2]
 }
